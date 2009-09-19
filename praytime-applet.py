d2395ec18e6a1bc5d669e74247fcad6716fcfa51
fuad <fuad1986@gmail.com>
2009-09-19 12:27:46 +0700

    update praytime-applet

--- praytime-applet.py
+++ praytime-applet.py
@@ -23,9 +23,61 @@ class PraytimeApplet:
     self.delta=self.getNextDeltaPrayTime()
     self.label = gtk.Label(self.parseDeltaPrayTime())
     applet.add(self.label)
+    self.create_menu()
     applet.show_all()
     gobject.timeout_add(self.timeout_interval, self.timeout_callback, self)
+
+  def create_menu(self):
+    xml="""
+    <popup name="button3">
+    <menuitem name="ItemPreferences" 
+    verb="Preferences" 
+    label="_Preferences" 
+    pixtype="stock" 
+    pixname="gtk-preferences"/>
+    </popup>"""
+    verbs = [('Preferences', self.show_preferences)]
+    self.applet.setup_menu(xml, verbs, None)
   
+  def show_preferences(self,*arguments):
+    dialog=gtk.Dialog('gPraytime Preferences')
+    ok_button = gtk.Button(stock=gtk.STOCK_OK)
+    dialog.action_area.pack_start(ok_button,True,True,0)
+    ok_button.show()
+    cancel_button = gtk.Button(stock=gtk.STOCK_CANCEL)
+    dialog.action_area.pack_start(cancel_button,True,True,0)
+    cancel_button.show()
+
+    table = gtk.Table(3,2,False)
+    
+    timezone_label = gtk.Label('Time Zone')
+    timezone_label.show()
+    table.attach(timezone_label,0,1,0,1)
+    timezone_entry = gtk.Entry()
+    timezone_entry.show()
+    table.attach(timezone_entry,1,2,0,1)
+    table.show()
+    
+    latitude_label = gtk.Label('Latitude')
+    latitude_label.show()
+    table.attach(latitude_label,0,1,1,2)
+    latitude_entry = gtk.Entry()
+    latitude_entry.show()
+    table.attach(latitude_entry,1,2,1,2)
+    table.show()
+
+    longitude_label = gtk.Label('Longitude')
+    longitude_label.show()
+    table.attach(longitude_label,0,1,2,3)
+    longitude_entry = gtk.Entry()
+    longitude_entry.show()
+    table.attach(longitude_entry,1,2,2,3)
+    table.show() 
+        
+
+    dialog.vbox.pack_start(table,True,True,0)
+    dialog.show()
+
   def parseDeltaPrayTime(self):
     hours = self.delta.seconds/3600
     minutes = (self.delta.seconds-(hours)*3600)/60
@@ -77,19 +129,26 @@ def praytime_factory(applet, iid):
     return gtk.TRUE
 
 #print "Starting factory"
-gnomeapplet.bonobo_factory("OAFIID:GNOME_PraytimeApplet_Factory", 
-                           gnomeapplet.Applet.__gtype__, 
-                           "PrayTime", "0", praytime_factory)
+#gnomeapplet.bonobo_factory("OAFIID:GNOME_PraytimeApplet_Factory", 
+#                           gnomeapplet.Applet.__gtype__, 
+#                           "PrayTime", "0", praytime_factory)
 #print "Factory ended"
 
-#if len(sys.argv) == 2 and sys.argv[1] == "run-in-window":   
-#        main_window = gtk.Window(gtk.WINDOW_TOPLEVEL)
-#        main_window.set_title("Python Applet")
-#        main_window.connect("destroy", gtk.main_quit) 
-#        app = gnomeapplet.Applet()
-#        praytime_factory(app, None)
-#        app.reparent(main_window)
-#        main_window.show_all()
-#        gtk.main()
-#        sys.exit()
+if len(sys.argv) == 2 and sys.argv[1] == "run-in-window":   
+  main_window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+  main_window.set_title("Python Applet")
+  main_window.connect("destroy", gtk.main_quit) 
+  app = gnomeapplet.Applet()
+  praytime_factory(app, None)
+  app.reparent(main_window)
+  main_window.show_all()
+  gtk.main()
+  sys.exit()
+else:
+  gnomeapplet.bonobo_factory("OAFIID:GNOME_PraytimeApplet_Factory",
+                           gnomeapplet.Applet.__gtype__,
+                           "PrayTime", "0", praytime_factory)
+
+  
+
 
