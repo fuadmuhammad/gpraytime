779bdadd289b8225e0f23d712fe1df5d01116aaa
fuad <fuad1986@gmail.com>
2009-09-23 08:27:43 +0700

    add preferences

--- praytime-applet.py
+++ praytime-applet.py
@@ -16,6 +16,13 @@ class PraytimeApplet:
   delta = 0
   label=None
   next_pray_name=None
+  timezone_entry=None
+  longitude_entry=None
+  latitude_entry=None
+  timezone = -6
+  longitude = 7
+  latitude = 106.650002
+  dialog=None
 
   def __init__(self,applet,iid):
     self.applet=applet
@@ -40,43 +47,61 @@ class PraytimeApplet:
     self.applet.setup_menu(xml, verbs, None)
   
   def show_preferences(self,*arguments):
-    dialog=gtk.Dialog('gPraytime Preferences')
+    self.timezone_entry=gtk.Entry()
+    self.timezone_entry.set_text(str(self.timezone))
+    self.longitude_entry=gtk.Entry()
+    self.longitude_entry.set_text(str(self.longitude))
+    self.latitude_entry=gtk.Entry()
+    self.latitude_entry.set_text(str(self.latitude))
+    self.dialog=gtk.Dialog('gPraytime Preferences')
     ok_button = gtk.Button(stock=gtk.STOCK_OK)
-    dialog.action_area.pack_start(ok_button,True,True,0)
+    self.dialog.action_area.pack_start(ok_button,True,True,0)
     ok_button.show()
+    ok_button.connect("clicked",self.ok_callback,"cool button")
     cancel_button = gtk.Button(stock=gtk.STOCK_CANCEL)
-    dialog.action_area.pack_start(cancel_button,True,True,0)
+    self.dialog.action_area.pack_start(cancel_button,True,True,0)
     cancel_button.show()
+    cancel_button.connect('clicked',self.cancel_callback,"cool button")
 
     table = gtk.Table(3,2,False)
     
     timezone_label = gtk.Label('Time Zone')
     timezone_label.show()
     table.attach(timezone_label,0,1,0,1)
-    timezone_entry = gtk.Entry()
-    timezone_entry.show()
-    table.attach(timezone_entry,1,2,0,1)
+    self.timezone_entry.show()
+    table.attach(self.timezone_entry,1,2,0,1)
     table.show()
     
     latitude_label = gtk.Label('Latitude')
     latitude_label.show()
     table.attach(latitude_label,0,1,1,2)
-    latitude_entry = gtk.Entry()
-    latitude_entry.show()
-    table.attach(latitude_entry,1,2,1,2)
+    self.latitude_entry.show()
+    table.attach(self.latitude_entry,1,2,1,2)
     table.show()
 
     longitude_label = gtk.Label('Longitude')
     longitude_label.show()
     table.attach(longitude_label,0,1,2,3)
-    longitude_entry = gtk.Entry()
-    longitude_entry.show()
-    table.attach(longitude_entry,1,2,2,3)
+    self.longitude_entry.show()
+    table.attach(self.longitude_entry,1,2,2,3)
     table.show() 
         
 
-    dialog.vbox.pack_start(table,True,True,0)
-    dialog.show()
+    self.dialog.vbox.pack_start(table,True,True,0)
+    self.dialog.show()
+
+  def ok_callback(self,widget,event):
+    self.timezone=int(self.timezone_entry.get_text())
+    self.latitude=float(self.latitude_entry.get_text())
+    self.longitude=int(self.longitude_entry.get_text()) 
+    self.delta=self.getNextDeltaPrayTime()
+    text_delta=self.parseDeltaPrayTime()
+    if self.label.get_label!=text_delta:
+      self.label.set_label(text_delta)
+    self.update_time()
+ 
+  def cancel_callback(self,widget,event):
+    self.dialog.destroy()
 
   def parseDeltaPrayTime(self):
     hours = self.delta.seconds/3600
@@ -86,20 +111,24 @@ class PraytimeApplet:
       return str(minutes)+":"+str(seconds)+" to "+self.next_pray_name
     else:
       return str(hours)+":"+str(minutes)+" to "+self.next_pray_name
- 
-  def timeout_callback(self,event):
-    if self.delta.seconds == 0:
+
+  def update_time(self):
+   if self.delta.seconds == 0:
       self.label.set_label("Now "+self.next_pray_name)
       self.delta=self.getNextDeltaPrayTime()
-    else:
+   else:
       text_delta=self.parseDeltaPrayTime()
       if self.label.get_label!=text_delta:
         self.label.set_label(text_delta)
       self.delta-=datetime.timedelta(0,1)
+ 
+  def timeout_callback(self,event):
+    self.update_time()
     return 1
   
   def getNextDeltaPrayTime(self):
-    praytimes = praytime.PrayTime(datetime.date.today(),-6 , 106.650002, 7)
+    #praytimes = praytime.PrayTime(datetime.date.today(),-6 , 106.650002, 7)
+    praytimes = praytime.PrayTime(datetime.date.today(),self.timezone,self.latitude,self.longitude)
     current_time = datetime.datetime.today() 
     current_hour = current_time.hour
     current_minute = current_time.minute
@@ -111,7 +140,7 @@ class PraytimeApplet:
     #praytime for fajr next day
     if not self.next_pray_name:
       tomorrow = datetime.date.today()+datetime.timedelta(1)
-      praytimes = praytime.PrayTime(tomorrow,-6,106.650002,7);
+      praytimes = praytime.PrayTime(tomorrow,self.timezone,self.latitude,self.longitude);
       praytimes = praytimes.getPrayTimes()
       next_praytime = datetime.datetime(tomorrow.year,tomorrow.month,tomorrow.day,praytimes[0]['hour'],praytimes[0]['minute'])
       self.next_pray_name=praytimes[0]['name']
